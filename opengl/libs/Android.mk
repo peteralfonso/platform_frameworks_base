29480969cf5f3c20d9b69bf3ba738b51eeeb865f
Gary King <gking@nvidia.com>
2010-03-04 17:13:36 -0800

    [opengl] implement work around for tegra errata 657451
    copy the tegra errata work around compile flags from bionic, to
    pick up the correct __get_tls implementation
    
    Change-Id: Ifccd4f5f382cc05328222c92da7f621faa1ec507

--- opengl/libs/Android.mk
+++ opengl/libs/Android.mk
@@ -31,6 +31,9 @@ else
         LOCAL_CFLAGS += -DHAVE_ARM_TLS_REGISTER
     endif
     # we need to access the private Bionic header <bionic_tls.h>
+    ifeq ($(TARGET_HAVE_TEGRA_ERRATA_657451),true)
+        LOCAL_CFLAGS += -DHAVE_TEGRA_ERRATA_657451
+    endif
     LOCAL_C_INCLUDES += bionic/libc/private
 endif
 
@@ -44,6 +47,9 @@ endif
 
 ifeq ($(ARCH_ARM_HAVE_TLS_REGISTER),true)
   LOCAL_CFLAGS += -DHAVE_ARM_TLS_REGISTER
+endif
+ifeq ($(TARGET_HAVE_TEGRA_ERRATA_657451),true)
+  LOCAL_CFLAGS += -DHAVE_TEGRA_ERRATA_657451
 endif
 
 include $(BUILD_SHARED_LIBRARY)
@@ -88,6 +94,9 @@ else
     ifeq ($(ARCH_ARM_HAVE_TLS_REGISTER),true)
         LOCAL_CFLAGS += -DHAVE_ARM_TLS_REGISTER
     endif
+    ifeq ($(TARGET_HAVE_TEGRA_ERRATA_657451),true)
+        LOCAL_CFLAGS += -DHAVE_TEGRA_ERRATA_657451
+    endif
     LOCAL_C_INCLUDES += bionic/libc/private
 endif
 
@@ -97,6 +106,9 @@ LOCAL_CFLAGS += -fvisibility=hidden
 
 ifeq ($(ARCH_ARM_HAVE_TLS_REGISTER),true)
   LOCAL_CFLAGS += -DHAVE_ARM_TLS_REGISTER
+endif
+ifeq ($(TARGET_HAVE_TEGRA_ERRATA_657451),true)
+  LOCAL_CFLAGS += -DHAVE_TEGRA_ERRATA_657451
 endif
 
 include $(BUILD_SHARED_LIBRARY)
@@ -124,6 +136,9 @@ else
     ifeq ($(ARCH_ARM_HAVE_TLS_REGISTER),true)
         LOCAL_CFLAGS += -DHAVE_ARM_TLS_REGISTER
     endif
+    ifeq ($(TARGET_HAVE_TEGRA_ERRATA_657451),true)
+        LOCAL_CFLAGS += -DHAVE_TEGRA_ERRATA_657451
+    endif
     LOCAL_C_INCLUDES += bionic/libc/private
 endif
 
@@ -133,6 +148,9 @@ LOCAL_CFLAGS += -fvisibility=hidden
 
 ifeq ($(ARCH_ARM_HAVE_TLS_REGISTER),true)
   LOCAL_CFLAGS += -DHAVE_ARM_TLS_REGISTER
+endif
+ifeq ($(TARGET_HAVE_TEGRA_ERRATA_657451),true)
+  LOCAL_CFLAGS += -DHAVE_TEGRA_ERRATA_657451
 endif
 
 include $(BUILD_SHARED_LIBRARY)
