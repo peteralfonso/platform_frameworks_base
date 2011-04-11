fc6bf68e1b63dcd6a7cda6c77b96851abf52a522
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2011-04-11 22:48:03 +0100

    Support old libaudiopolicy binaries
    The stream strategy and effects methods are new on 2.3, and invoking them
    on old audiopolicy proprietaries results in bad things.
    Especially if those proprietaries have other non-standard functions
    inserted into the interface, in the same locations this code expects to
    find effects-related things...
    
    Enable with BOARD_USES_FROYO_AUDIOPOLICY
    
    Change-Id: I3564862b31d44394a99e37767c767ce1a53d77a1

--- services/audioflinger/Android.mk
+++ services/audioflinger/Android.mk
@@ -114,6 +114,10 @@ ifeq ($(AUDIO_POLICY_TEST),true)
   LOCAL_CFLAGS += -DAUDIO_POLICY_TEST
 endif
 
+ifeq ($(BOARD_USES_FROYO_AUDIOPOLICY),true)
+  LOCAL_CFLAGS += -DFROYO_AUDIOPOLICY
+endif
+
 ifeq ($(TARGET_SIMULATOR),true)
     ifeq ($(HOST_OS),linux)
         LOCAL_LDLIBS += -lrt -lpthread
