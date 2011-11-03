CyanogenMod/android_frameworks_base

sensors: Add dummy light sensor for p990/p999
This is necessary for the rest of userspace to recognize its existence
and toggle it for auto-brightness. It doesn't generate actual light
values, though; all backlight adjustments are made in-kernel.

Change-Id: I0e546b4740720bd34d1e1d85a96b295fcc697106

services/sensorservice/SensorDevice.cpp
@@ -30,6 +30,10 @@
 
 #include "SensorDevice.h"
 
+#ifdef USE_LGE_ALS_DUMMY
+#include <fcntl.h>
+#endif
+
 namespace android {
 // ---------------------------------------------------------------------------
 class BatteryService : public Singleton<BatteryService> {
@@ -97,6 +101,27 @@ ANDROID_SINGLETON_STATIC_INSTANCE(BatteryService)
 
 ANDROID_SINGLETON_STATIC_INSTANCE(SensorDevice)
 
+#ifdef USE_LGE_ALS_DUMMY
+static ssize_t addDummyLGESensor(sensor_t const **list, ssize_t count) {
+    struct sensor_t dummy_light =     {
+                  name            : "Dummy LGE-Star light sensor",
+                  vendor          : "CyanogeMmod",
+                  version         : 1,
+                  handle          : SENSOR_TYPE_LIGHT,
+                  type            : SENSOR_TYPE_LIGHT,
+                  maxRange        : 20,
+                  resolution      : 0.1,
+                  power           : 20,
+    };
+    void * new_list = malloc((count+1)*sizeof(sensor_t));
+    new_list = memcpy(new_list, *list, count*sizeof(sensor_t));
+    ((sensor_t *)new_list)[count] = dummy_light;
+    *list = (sensor_t const *)new_list;
+    count++;
+    return count;
+}
+#endif
+
 SensorDevice::SensorDevice()
     :  mSensorDevice(0),
        mOldSensorsEnabled(0),
@@ -139,15 +164,23 @@ SensorDevice::SensorDevice()
         if (mSensorDevice || mOldSensorsCompatMode) {
             sensor_t const* list;
             ssize_t count = mSensorModule->get_sensors_list(mSensorModule, &list);
+
+#ifdef USE_LGE_ALS_DUMMY
+            count = addDummyLGESensor(&list, count);
+#endif
+
+            if (mOldSensorsCompatMode) {
+                mOldSensorsList = list;
+                mOldSensorsCount = count;
+                mSensorDataDevice->data_open(mSensorDataDevice,
+                            mSensorControlDevice->open_data_source(mSensorControlDevice));
+            }
+
             mActivationCount.setCapacity(count);
             Info model;
             for (size_t i=0 ; i<size_t(count) ; i++) {
                 mActivationCount.add(list[i].handle, model);
                 if (mOldSensorsCompatMode) {
-                    mOldSensorsList = list;
-                    mOldSensorsCount = count;
-                    mSensorDataDevice->data_open(mSensorDataDevice,
-                            mSensorControlDevice->open_data_source(mSensorControlDevice));
                     mSensorControlDevice->activate(mSensorControlDevice, list[i].handle, 0);
                 } else {
                     mSensorDevice->activate(mSensorDevice, list[i].handle, 0);
@@ -178,6 +211,10 @@ void SensorDevice::dump(String8& result, char* buffer, size_t SIZE)
 ssize_t SensorDevice::getSensorList(sensor_t const** list) {
     if (!mSensorModule) return NO_INIT;
     ssize_t count = mSensorModule->get_sensors_list(mSensorModule, list);
+
+#ifdef USE_LGE_ALS_DUMMY
+    return addDummyLGESensor(list, count);
+#endif
     return count;
 }
 
@@ -240,9 +277,9 @@ ssize_t SensorDevice::poll(sensors_event_t* buffer, size_t count) {
                     buffer[pollsDone].distance = 1;
                 }
 #endif
-		return pollsDone+1;
+                return pollsDone+1;
             } else if (sensorType == SENSOR_TYPE_LIGHT) {
-		return pollsDone+1;
+                return pollsDone+1;
             }
             pollsDone++;
         }
@@ -258,6 +295,32 @@ status_t SensorDevice::activate(void* ident, int handle, int enabled)
     status_t err(NO_ERROR);
     bool actuateHardware = false;
 
+#ifdef USE_LGE_ALS_DUMMY
+
+    if (handle == SENSOR_TYPE_LIGHT) {
+        int nwr, ret, fd;
+        char value[2];
+
+
+        fd = open("/sys/devices/platform/star_aat2870.0/lsensor_onoff", O_RDWR);
+        if(fd < 0)
+            return -ENODEV;
+
+        nwr = sprintf(value, "%s\n", enabled ? "1" : "0");
+        write(fd, value, nwr);
+        close(fd);
+        fd = open("/sys/devices/platform/star_aat2870.0/alc", O_RDWR);
+        if(fd < 0)
+            return -ENODEV;
+
+        nwr = sprintf(value, "%s\n", enabled ? "2" : "0");
+        write(fd, value, nwr);
+        close(fd);
+
+        return 0;
+
+    }
+#endif
     Info& info( mActivationCount.editValueFor(handle) );
     if (enabled) {
         Mutex::Autolock _l(mLock);

peteralfonso/platform_frameworks_base

services/sensorservice/SensorDevice.cpp
@@ -30,6 +30,10 @@
 
 #include "SensorDevice.h"
 
+#ifdef USE_LGE_ALS_DUMMY
+#include <fcntl.h>
+#endif
+
 namespace android {
 // ---------------------------------------------------------------------------
 class BatteryService : public Singleton<BatteryService> {
@@ -97,6 +101,27 @@ ANDROID_SINGLETON_STATIC_INSTANCE(BatteryService)
 
 ANDROID_SINGLETON_STATIC_INSTANCE(SensorDevice)
 
+#ifdef USE_LGE_ALS_DUMMY
+static ssize_t addDummyLGESensor(sensor_t const **list, ssize_t count) {
+    struct sensor_t dummy_light =     {
+                  name            : "Dummy LGE-Star light sensor",
+                  vendor          : "CyanogenMod",
+                  version         : 1,
+                  handle          : SENSOR_TYPE_LIGHT,
+                  type            : SENSOR_TYPE_LIGHT,
+                  maxRange        : 20,
+                  resolution      : 0.1,
+                  power           : 20,
+    };
+    void * new_list = malloc((count+1)*sizeof(sensor_t));
+    new_list = memcpy(new_list, *list, count*sizeof(sensor_t));
+    ((sensor_t *)new_list)[count] = dummy_light;
+    *list = (sensor_t const *)new_list;
+    count++;
+    return count;
+}
+#endif
+
 SensorDevice::SensorDevice()
     :  mSensorDevice(0),
        mOldSensorsEnabled(0),
@@ -139,15 +164,23 @@ SensorDevice::SensorDevice()
         if (mSensorDevice || mOldSensorsCompatMode) {
             sensor_t const* list;
             ssize_t count = mSensorModule->get_sensors_list(mSensorModule, &list);
+
+#ifdef USE_LGE_ALS_DUMMY
+            count = addDummyLGESensor(&list, count);
+#endif
+
+            if (mOldSensorsCompatMode) {
+                mOldSensorsList = list;
+                mOldSensorsCount = count;
+                mSensorDataDevice->data_open(mSensorDataDevice,
+                            mSensorControlDevice->open_data_source(mSensorControlDevice));
+            }
+
             mActivationCount.setCapacity(count);
             Info model;
             for (size_t i=0 ; i<size_t(count) ; i++) {
                 mActivationCount.add(list[i].handle, model);
                 if (mOldSensorsCompatMode) {
-                    mOldSensorsList = list;
-                    mOldSensorsCount = count;
-                    mSensorDataDevice->data_open(mSensorDataDevice,
-                            mSensorControlDevice->open_data_source(mSensorControlDevice));
                     mSensorControlDevice->activate(mSensorControlDevice, list[i].handle, 0);
                 } else {
                     mSensorDevice->activate(mSensorDevice, list[i].handle, 0);
@@ -178,6 +211,10 @@ void SensorDevice::dump(String8& result, char* buffer, size_t SIZE)
 ssize_t SensorDevice::getSensorList(sensor_t const** list) {
     if (!mSensorModule) return NO_INIT;
     ssize_t count = mSensorModule->get_sensors_list(mSensorModule, list);
+
+#ifdef USE_LGE_ALS_DUMMY
+    return addDummyLGESensor(list, count);
+#endif
     return count;
 }
 
@@ -240,9 +277,9 @@ ssize_t SensorDevice::poll(sensors_event_t* buffer, size_t count) {
                     buffer[pollsDone].distance = 1;
                 }
 #endif
-		return pollsDone+1;
+                return pollsDone+1;
             } else if (sensorType == SENSOR_TYPE_LIGHT) {
-		return pollsDone+1;
+                return pollsDone+1;
             }
             pollsDone++;
         }
@@ -258,6 +295,32 @@ status_t SensorDevice::activate(void* ident, int handle, int enabled)
     status_t err(NO_ERROR);
     bool actuateHardware = false;
 
+#ifdef USE_LGE_ALS_DUMMY
+
+    if (handle == SENSOR_TYPE_LIGHT) {
+        int nwr, ret, fd;
+        char value[2];
+
+
+        fd = open("/sys/devices/platform/star_aat2870.0/lsensor_onoff", O_RDWR);
+        if(fd < 0)
+            return -ENODEV;
+
+        nwr = sprintf(value, "%s\n", enabled ? "1" : "0");
+        write(fd, value, nwr);
+        close(fd);
+        fd = open("/sys/devices/platform/star_aat2870.0/alc", O_RDWR);
+        if(fd < 0)
+            return -ENODEV;
+
+        nwr = sprintf(value, "%s\n", enabled ? "2" : "0");
+        write(fd, value, nwr);
+        close(fd);
+
+        return 0;
+
+    }
+#endif
     Info& info( mActivationCount.editValueFor(handle) );
     if (enabled) {
         Mutex::Autolock _l(mLock);